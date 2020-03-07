d8e5f21398a9ce6a75dea63f2a6530bb6b79f2ba
Lukas Wais <lukas.wais@outlook.de>
2020-03-07 14:35:06 +0100

    Deleted Design Patterns

--- Documents/DesignPatterns/src/adapter/IPersonenLeser.java
+++
@@ -1,9 +0,0 @@
-package Adapter;
-
-// Datei: IPersonenLeser.java
-import java.util.Vector;
-
-public interface IPersonenLeser {
-	// hat als Rueckgabewert einen Vektor von Personen
-	public Vector<Person> lesePersonen();
-}
