168b9390ccad5352c0c1e431eaf17f014aba7e88
Lukas <lukas.wais@outlook.de>
2020-04-08 23:51:28 +0200

    Added first Maven Project, with Google Guava

---
+++ Sourcecode/FirstMavenProject/Main.java
@@ -0,0 +1,8 @@
+import static com.google.common.math.LongMath.isPrime;
+
+public class Main {
+    @SuppressWarnings("UnstableApiUsage")
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+}
